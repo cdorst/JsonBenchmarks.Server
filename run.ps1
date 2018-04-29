463b4a89d220bf666e1cbb7b27c46d49feecdfb8
Christopher Dorst <chris@dorst.email>
2018-04-29 08:13:44 -0500

    Use $server variable for container name

--- run.ps1
+++ run.ps1
@@ -1,8 +1,9 @@
-& docker stop jsonbenchmarksserver_server_1
-& docker rm jsonbenchmarksserver_server_1
+$server = "jsonbenchmarksserver_server_1"
+& docker stop $server
+& docker rm $server
 & docker-compose up --build --detach
 & cd Benchmarks
 & dotnet run -c Release
-& docker stop jsonbenchmarksserver_server_1
-& docker rm jsonbenchmarksserver_server_1
+& docker stop $server
+& docker rm $server
 & cd ..
